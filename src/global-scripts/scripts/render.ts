65a46063681b28a994f3619a77b2c4543f94dbbd
yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>
2023-03-16 23:24:58 +0100

    fix: Fixed footer links

--- src/global-scripts/scripts/render.ts
+++ src/global-scripts/scripts/render.ts
@@ -1,7 +1,7 @@
 import { addMenu } from '../../components/menu/menu';
-// import { addFooter } from '../../components/footer/footer';
+import { addFooter } from '../../components/footer/footer';
 
 export function render() {
   addMenu();
-  // addFooter();
+  addFooter();
 }
